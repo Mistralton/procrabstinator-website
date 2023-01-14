1cda6ca2c1a9b894f259382c6249fd86cfddacbb
Wesley <60171023+wesngu28@users.noreply.github.com>
2023-01-14 12:49:20 -0800

    testing extension

--- extensions/firefox/index.js
+++ extensions/firefox/index.js
@@ -1,9 +1,18 @@
-setInterval(getAllTabs, 30000);
+setInterval(getAllTabs, 10000);
 
-function logTabs(tabs) {
+async function logTabs(tabs) {
+  let tabArr = []
   for (const tab of tabs) {
-    console.log(tab.url);
+    tabArr.push(tab.url)
   }
+  const testFetch = await fetch('http://localhost:5000/', {
+    method: 'POST',
+    mode: 'cors',
+    body: JSON.stringify(tabArr),
+  })
+  console.log(testFetch)
+  const testFetchJson = await testFetch.json()
+  console.log(testFetchJson)
 }
 
 function onError(error) {
